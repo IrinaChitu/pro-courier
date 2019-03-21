2473d984ae5fb0ce861ba64f9642b08f0d7fd421
Irina <chitu.irina.nicoleta@gmail.com>
2019-03-21 20:24:42 +0200

    Updated the OrderService

--- orders-service/src/main/java/com/procourier/app/OrderServiceMain.java
+++ orders-service/src/main/java/com/procourier/app/OrderServiceMain.java
@@ -1,12 +1,36 @@
 package com.procourier.app;
 
+import com.procourier.model.*;
+import com.procourier.repository.CollectionRepository;
 import spark.Spark;
+
+import java.util.Map;
 
 public final class OrderServiceMain {
     public static void main(String[] args) {    // psvm -> genereaza main
+        final var repository = new CollectionRepository();
+        final var handler = new OrdersHandler(repository);
+
+        final Address sellerAddress = new Address("DJ", "Craiova", "Nicolae Titulescu");
+        final Seller seller = new Seller("Evomag", sellerAddress);
+
+        final Address buyerAddress = new Address("DJ", "Craiova", "Ileana Cosanzeana");
+        final Buyer buyer = new Buyer("Cosmin", buyerAddress);
+
+        final Address courierAddress = new Address("OT", "Slatina", "Cantemir");
+        final Courier courier = new Courier(1L, "ExpressFan", courierAddress, 5L, 7L);
+
+
+        final var products = Map.of(new Product(20L, "Jeans", "Men Jeans", 22L), 4);
+
+        final Order order = new Order(6L, seller, buyer, courier, products);
+
+        repository.put(order);
         Spark.port(8082);
+        Spark.threadPool(26, 10, 2000); // nr de fire de executie care pot raspunde la request-uri
         //Spark.get("/orders/hello", (request, response) -> "Hello World!"); // inainte de implementarea clasei OrdersHandler
         //Spark.get("/orders/hello", new OrdersHandler());  // inainte de a face constructorul privat
-        Spark.get("/orders/hello", OrdersHandler.INSTANCE);
+        //Spark.get("/orders/hello", OrdersHandler.INSTANCE);   // inainte de a face contructorul public si a rescrie metoda handler in OrdersHandler
+        Spark.get("/orders/:ID", handler);
     }
 }
--- orders-service/src/main/java/com/procourier/app/OrdersHandler.java
+++ orders-service/src/main/java/com/procourier/app/OrdersHandler.java
@@ -1,18 +1,35 @@
 package com.procourier.app;
 
+import com.procourier.model.Order;
+import com.procourier.model.repository.OrderRepository;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
-public final class OrdersHandler implements Route {
-    private OrdersHandler() {
+import java.util.Objects;
+import java.util.Optional;
 
+public final class OrdersHandler implements Route {
+    private final OrderRepository repository;
+
+    //private OrdersHandler() {}
+    //public static final OrdersHandler INSTANCE = new OrdersHandler();
+
+    public OrdersHandler(OrderRepository repository) {
+        this.repository = Objects.requireNonNull(repository);
     }
 
-    public static final OrdersHandler INSTANCE = new OrdersHandler();
+
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        return "Hello World";
+        //return "Hello World";
+        final String ID = request.params(":ID");
+        final Long longID = Long.valueOf(ID);
+
+        final Optional<Order> order = repository.findByID(longID);
+        final String info = order.map(o -> o.getID() + " " + o.getBuyer().getName())
+                                    .orElse("Not found");
+        return info;
     }
 }
